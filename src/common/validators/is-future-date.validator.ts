5fcda595ae830dee11350bee0d7d7c4c2b4dbc58
Elijah Arhinful <elijaharhinful8@gmail.com>
2026-06-12 22:34:58 +0000

    feat: add date validator tp job creation

--- src/common/validators/is-future-date.validator.ts
+++ src/common/validators/is-future-date.validator.ts
@@ -1,4 +1,9 @@
-import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
 
 @ValidatorConstraint({ name: 'isFutureDate', async: false })
 export class IsFutureDateConstraint implements ValidatorConstraintInterface {
